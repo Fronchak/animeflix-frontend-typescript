2a7a73b7802fb526bdbaf99c15aef6cbea2a382e
Gabriel Fronchak Gmack <gabrielgmack@gmail.com>
2023-02-05 11:21:48 -0300

    Fix navbar layout

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -18,7 +18,7 @@ const Navbar = ({ authenticated, tokenData }: Props) => {
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarItems">
-          <ul className="navbar-nav mb-2 mb-lg-0">
+          <ul className="navbar-nav mb-0">
             <li className="nav-item">
               <NavLink className="nav-link" aria-current="page" to="/">Home</NavLink>
             </li>
@@ -30,9 +30,12 @@ const Navbar = ({ authenticated, tokenData }: Props) => {
             </li>
           </ul>
           <ul className="navbar-nav mb-2 mb-lg-0">
-            <li className="nav-item">
-              <span className="nav-link text-white">{ tokenData?.user_name }</span>
-            </li>
+            { authenticated && (
+              <li className="nav-item">
+                <span className="nav-link text-white">{ tokenData?.user_name }</span>
+              </li>
+            ) }
+
             <li className="nav-item">
               { authenticated ? (
                 <>
